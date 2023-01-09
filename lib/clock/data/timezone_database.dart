8d7ac064692c55195ca86b0cc17a00bb71f0b3ce
AhsanSarwar45 <ahsansarwar.as45@gmail.com>
2023-01-09 18:08:35 +0500

    Remove unnecessary packages

--- lib/clock/data/timezone_database.dart
+++ lib/clock/data/timezone_database.dart
@@ -1,32 +1,20 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'package:clock_app/common/data/paths.dart';
 // Database? database;
 
 Future<void> initializeDatabases() async {
-  // If Clock directory doesn't exist, create it
-
   String appDataPath = await getAppDataDirectoryPath();
 
   if (!await Directory(appDataPath).exists()) {
     await Directory(appDataPath).create();
   }
 
-  // print('Database path: ${await getMainDatabasePath()}');
-
-  // database = await openDatabase(await getMainDatabasePath(), version: 1,
-  //     onCreate: (Database db, int version) async {
-  //   await db.execute(
-  //       'CREATE TABLE IF NOT EXISTS SavedCities (City String, Timezone String, Country String)');
-  // });
-
   String timezonesDatabasePath = await getTimezonesDatabasePath();
 
-// Only copy if the database doesn't exist
+  // Only copy if the database doesn't exist
   if (FileSystemEntity.typeSync(timezonesDatabasePath) ==
       FileSystemEntityType.notFound) {
     // Load database from asset and copy
@@ -36,13 +24,5 @@ Future<void> initializeDatabases() async {
 
     // Save copied asset to documents
     await File(timezonesDatabasePath).writeAsBytes(bytes);
-  }
-
-  // sqflite doesn't support Windows and Linux, so we use sqflite_ffi
-  // This works on debug builds but for release build on Windows, we need to copy
-  // the sqlite3.dll file next to the executable
-  if (Platform.isWindows || Platform.isLinux) {
-    sqfliteFfiInit();
-    databaseFactory = databaseFactoryFfi;
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import audio_service
 import audio_session
 import just_audio
 import path_provider_macos
@@ -13,7 +12,6 @@ import shared_preferences_macos
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
