ca986f91199e7e2bf79899d036cf50188799df17
AhsanSarwar45 <ahsansarwar.as45@gmail.com>
2023-12-12 14:21:43 +0500

    Fix slider and switch themes not changing

--- lib/theme/utils/color_scheme.dart
+++ lib/theme/utils/color_scheme.dart
@@ -1,6 +1,8 @@
 import 'package:clock_app/settings/data/settings_schema.dart';
 import 'package:clock_app/theme/bottom_sheet.dart';
 import 'package:clock_app/theme/popup_menu.dart';
+import 'package:clock_app/theme/slider.dart';
+import 'package:clock_app/theme/switch.dart';
 import 'package:clock_app/theme/types/color_scheme.dart';
 import 'package:clock_app/theme/input.dart';
 import 'package:clock_app/theme/radio.dart';
@@ -17,18 +19,19 @@ ThemeData getThemeFromColorScheme(
       .value;
 
   return theme.copyWith(
-    colorScheme: getColorScheme(colorSchemeData),
-    scaffoldBackgroundColor: colorSchemeData.background,
-    cardColor: colorSchemeData.card,
-    radioTheme: getRadioTheme(colorSchemeData),
-    dialogBackgroundColor: colorSchemeData.card,
-    bottomSheetTheme: getBottomSheetTheme(colorSchemeData, styleTheme),
-    textTheme: theme.textTheme.apply(
-      bodyColor: colorSchemeData.onBackground,
-      displayColor: colorSchemeData.onBackground,
-    ),
-    snackBarTheme: getSnackBarTheme(colorSchemeData, styleTheme),
-    inputDecorationTheme: getInputTheme(colorSchemeData, styleTheme),
-    popupMenuTheme: getPopupMenuTheme(colorSchemeData, styleTheme),
-  );
+      colorScheme: getColorScheme(colorSchemeData),
+      scaffoldBackgroundColor: colorSchemeData.background,
+      cardColor: colorSchemeData.card,
+      radioTheme: getRadioTheme(colorSchemeData),
+      dialogBackgroundColor: colorSchemeData.card,
+      bottomSheetTheme: getBottomSheetTheme(colorSchemeData, styleTheme),
+      textTheme: theme.textTheme.apply(
+        bodyColor: colorSchemeData.onBackground,
+        displayColor: colorSchemeData.onBackground,
+      ),
+      snackBarTheme: getSnackBarTheme(colorSchemeData, styleTheme),
+      inputDecorationTheme: getInputTheme(colorSchemeData, styleTheme),
+      popupMenuTheme: getPopupMenuTheme(colorSchemeData, styleTheme),
+      switchTheme: getSwitchTheme(colorSchemeData),
+      sliderTheme: getSliderTheme(colorSchemeData));
 }
