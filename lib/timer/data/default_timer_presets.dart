99861d91a9006a7e5f559b0a1acd89bf640a992d
AhsanSarwar45 <ahsansarwar.as45@gmail.com>
2023-12-13 10:29:01 +0500

    Change default timer presets

--- lib/timer/data/default_timer_presets.dart
+++ lib/timer/data/default_timer_presets.dart
@@ -4,8 +4,8 @@ import 'package:clock_app/timer/types/timer_preset.dart';
 List<TimerPreset> defaultTimerPresets = [
   TimerPreset("1 min", const TimeDuration(minutes: 1)),
   TimerPreset("5 min", const TimeDuration(minutes: 5)),
+  TimerPreset("10 min", const TimeDuration(minutes: 10)),
   TimerPreset("Workout", const TimeDuration(minutes: 10)),
   TimerPreset("Meditation", const TimeDuration(minutes: 15)),
-  TimerPreset("10 min", const TimeDuration(minutes: 10)),
   TimerPreset("Sleep", const TimeDuration(hours: 5)),
 ];
