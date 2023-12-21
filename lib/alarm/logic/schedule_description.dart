641178a8a0e1837833d8def2bb8dedee668ac5fb
AhsanSarwar45 <ahsansarwar.as45@gmail.com>
2023-12-21 07:45:51 +0500

    Update tests

--- lib/alarm/logic/schedule_description.dart
+++ lib/alarm/logic/schedule_description.dart
@@ -18,7 +18,7 @@ String getAlarmScheduleDescription(BuildContext context, Alarm alarm,
     String dateFormat, TimeFormat timeFormat) {
   String suffix = '';
   if (alarm.shouldSkipNextAlarm) {
-    suffix = '(skipping next occurence)';
+    suffix = ' (skipping next occurence)';
   }
   if (alarm.isSnoozed) {
     return 'Snoozed until ${DateFormat(getTimeFormatString(context, timeFormat)).format(alarm.snoozeTime!)}';
@@ -31,27 +31,27 @@ String getAlarmScheduleDescription(BuildContext context, Alarm alarm,
   }
   switch (alarm.scheduleType) {
     case OnceAlarmSchedule:
-      return 'Just ${alarm.time.toHours() > DateTime.now().toHours() ? 'today' : 'tomorrow'} $suffix';
+      return 'Just ${alarm.time.toHours() > DateTime.now().toHours() ? 'today' : 'tomorrow'}$suffix';
     case DailyAlarmSchedule:
-      return 'Every day $suffix';
+      return 'Every day$suffix';
     case WeeklyAlarmSchedule:
       List<Weekday> alarmWeekdays = alarm.weekdays;
       if (alarmWeekdays.length == 7) {
-        return 'Every day $suffix';
+        return 'Every day$suffix';
       }
       if (alarmWeekdays.length == 2 &&
           weekdaysContainsAll(alarmWeekdays, ['Sat', 'Sun'])) {
-        return 'Every weekend $suffix';
+        return 'Every weekend$suffix';
       }
       if (alarmWeekdays.length == 5 &&
           weekdaysContainsAll(
               alarmWeekdays, ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'])) {
-        return 'Every weekday $suffix';
+        return 'Every weekday$suffix';
       }
-      return 'Every ${weekdays.where((weekday) => alarmWeekdays.contains(weekday)).map((weekday) => weekday.displayName).join(', ')} $suffix';
+      return 'Every ${weekdays.where((weekday) => alarmWeekdays.contains(weekday)).map((weekday) => weekday.displayName).join(', ')}$suffix';
     case DatesAlarmSchedule:
       List<DateTime> dates = alarm.dates;
-      return 'On ${DateFormat(dateFormat).format(dates[0])}${dates.length > 1 ? ' and ${dates.length - 1} other date${dates.length > 2 ? 's' : ''} ' : ''} $suffix';
+      return 'On ${DateFormat(dateFormat).format(dates[0])}${dates.length > 1 ? ' and ${dates.length - 1} other date${dates.length > 2 ? 's' : ''} ' : ''}$suffix';
     case RangeAlarmSchedule:
       DateTime rangeStart = alarm.startDate;
       DateTime rangeEnd = alarm.endDate;
@@ -73,7 +73,7 @@ String getAlarmScheduleDescription(BuildContext context, Alarm alarm,
         }
       }
 
-      return '${interval == RangeInterval.daily ? "Daily" : "Weekly"} from $startString to $endString $suffix';
+      return '${interval == RangeInterval.daily ? "Daily" : "Weekly"} from $startString to $endString$suffix';
     default:
       return 'Not scheduled';
   }
