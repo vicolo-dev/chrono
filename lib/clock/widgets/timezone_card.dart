e80599bd7fa16441242aa7202201aca71bdd7b6b
AhsanSarwar45 <ahsansarwar.as45@example.com>
2024-04-03 14:15:36 +0500

    Change layout of city cards

--- lib/clock/widgets/timezone_card.dart
+++ lib/clock/widgets/timezone_card.dart
@@ -1,13 +1,18 @@
 import 'package:clock_app/clock/types/city.dart';
 import 'package:clock_app/clock/widgets/timezone_card_content.dart';
+import 'package:clock_app/common/utils/popup_action.dart';
+import 'package:clock_app/common/widgets/card_edit_menu.dart';
+import 'package:clock_app/common/widgets/clock/clock.dart';
 import 'package:flutter/material.dart';
+import 'package:timer_builder/timer_builder.dart';
 import 'package:timezone/timezone.dart' as timezone;
 
 class TimeZoneCard extends StatelessWidget {
   TimeZoneCard({
-    Key? key,
+    super.key,
     required this.city,
-  }) : super(key: key) {
+    required this.onDelete,
+  }) {
     _timezoneLocation = timezone.getLocation(city.timezone);
     _offset = (_timezoneLocation.currentTimeZone.offset -
             DateTime.now().timeZoneOffset.inMilliseconds) /
@@ -16,6 +21,7 @@ class TimeZoneCard extends StatelessWidget {
 
   late final timezone.Location _timezoneLocation;
   late final double _offset;
+  final VoidCallback onDelete;
   final City city;
 
   String _formatTimeOffset(double n) {
@@ -27,9 +33,9 @@ class TimeZoneCard extends StatelessWidget {
     DateTime cityTime = timezone.TZDateTime.now(_timezoneLocation);
 
     String hourDifference = _formatTimeOffset(_offset.abs());
-    String hourLabel = _offset == 1 ? 'hour' : 'hours';
+    String hourLabel = _offset == 1 ? 'h' : 'h';
     String relativeLabel = _offset < 0 ? 'behind' : 'ahead';
-    String differentOffsetLabel = '$hourDifference $hourLabel $relativeLabel';
+    String differentOffsetLabel = '$hourDifference$hourLabel $relativeLabel';
     String offsetLabel = _offset != 0 ? differentOffsetLabel : 'Same time';
 
     String differentDayLabel = currentTime.day < cityTime.day
@@ -38,15 +44,75 @@ class TimeZoneCard extends StatelessWidget {
             ? ' (previous day)'
             : '';
 
-    return '$offsetLabel$differentDayLabel';
+    return offsetLabel;
   }
 
   @override
   Widget build(BuildContext context) {
-    return TimezoneCardContent(
-      title: city.name,
-      subtitle: _getOffsetDescription(),
-      timezoneLocation: _timezoneLocation,
+    ThemeData theme = Theme.of(context);
+    ColorScheme colorScheme = theme.colorScheme;
+    TextTheme textTheme = theme.textTheme;
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: 16.0, top: 8.0, bottom: 8.0, right: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Clock(
+                  timezoneLocation: _timezoneLocation,
+                  scale: 0.4,
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  city.name,
+                  style: textTheme.displaySmall
+                      ?.copyWith(color: colorScheme.onSurface.withOpacity(0.8)),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  softWrap: false,
+                ),
+                const SizedBox(height: 4),
+              ],
+            ),
+          ),
+        ),
+        // const SizedBox(width: 8),
+        Padding(
+          padding: const EdgeInsets.only(right: 16.0, top: 12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TimerBuilder.periodic(
+                const Duration(seconds: 1),
+                builder: (context) {
+                  return Text(
+                    _getOffsetDescription(),
+                    style: textTheme.bodyMedium?.copyWith(
+                        height: 0.5,
+                        color: colorScheme.onSurface.withOpacity(0.8)),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: false,
+                  );
+                },
+              ),
+              CardEditMenu(actions: [
+                getDeletePopupAction(context, onDelete),
+              ]),
+            ],
+          ),
+        ),
+      ],
     );
+
+    // TimezoneCardContent(
+    //   title: city.name,
+    //   subtitle: _getOffsetDescription(),
+    //   timezoneLocation: _timezoneLocation,
+    //   onPressDelete: onDelete,
+    // );
   }
 }
