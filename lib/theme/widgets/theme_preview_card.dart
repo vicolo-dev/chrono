979e3fcbe75b763f2a52e3e14aa206d375b0ada2
AhsanSarwar45 <ahsansarwar.as45@example.com>
2024-05-12 00:05:43 +0500

    Add wrapping to theme preview card

--- lib/theme/widgets/theme_preview_card.dart
+++ lib/theme/widgets/theme_preview_card.dart
@@ -20,24 +20,24 @@ class ThemePreviewCard extends StatelessWidget {
       color: colorScheme.background,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Text(
-                AppLocalizations.of(context)!.previewLabel,
-                style: textTheme.headlineMedium?.copyWith(
-                  color: colorScheme.onBackground,
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  AppLocalizations.of(context)!.previewLabel,
+                  style: textTheme.headlineMedium?.copyWith(
+                    color: colorScheme.onBackground,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: CardContainer(
+              const SizedBox(height: 8.0),
+              Wrap(
+                children: [
+                  CardContainer(
                     color: getCardColor(context),
                     key: const Key("Preview Card - Card"),
                     child: Padding(
@@ -50,30 +50,30 @@ class ThemePreviewCard extends StatelessWidget {
                       ),
                     ),
                   ),
-                ),
-                CardContainer(
-                  key: const Key("Preview Card - Accent"),
-                  color: colorScheme.primary,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(AppLocalizations.of(context)!.accentLabel,
-                        style: textTheme.bodyMedium
-                            ?.copyWith(color: colorScheme.onPrimary)),
+                  CardContainer(
+                    key: const Key("Preview Card - Accent"),
+                    color: colorScheme.primary,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(AppLocalizations.of(context)!.accentLabel,
+                          style: textTheme.bodyMedium
+                              ?.copyWith(color: colorScheme.onPrimary)),
+                    ),
                   ),
-                ),
-                CardContainer(
-                  key: const Key("Preview Card - Error"),
-                  color: colorScheme.error,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(AppLocalizations.of(context)!.errorLabel,
-                        style: textTheme.bodyMedium
-                            ?.copyWith(color: colorScheme.onError)),
-                  ),
-                )
-              ],
-            ),
-          ],
+                  CardContainer(
+                    key: const Key("Preview Card - Error"),
+                    color: colorScheme.error,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(AppLocalizations.of(context)!.errorLabel,
+                          style: textTheme.bodyMedium
+                              ?.copyWith(color: colorScheme.onError)),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
